e8dc15e0d5aee14bea1cd0cc092aaa248ee508d4
denis <denis@begom.com>
2024-03-30 11:19:10 +0100

    crypto

---
+++ crypto/publickey.go
@@ -0,0 +1,40 @@
+package crypto
+
+import (
+	"bytes"
+	"crypto/ed25519"
+	"encoding/base64"
+	"strings"
+)
+
+type PublicKey []byte
+
+const PublicKeySize = ed25519.PublicKeySize
+
+const publicKeyEncodingPrefix = "Ed25519,"
+
+func (pub PublicKey) String() string {
+	return pub.Encode()
+}
+
+func (pub PublicKey) Encode() string {
+	return publicKeyEncodingPrefix + base64.StdEncoding.EncodeToString(pub)
+}
+
+func (pub PublicKey) Equal(p PublicKey) bool {
+	return len(pub) == PublicKeySize && bytes.Equal(pub, p)
+}
+
+func (pub PublicKey) Verify(message, signature []byte) bool {
+	return len(pub) == PublicKeySize &&
+		len(signature) == SignatureSize &&
+		ed25519.Verify([]byte(pub), message, signature)
+}
+
+func DecodePublicKey(s string) PublicKey {
+	s = strings.TrimPrefix(s, publicKeyEncodingPrefix)
+	if p, _ := base64.StdEncoding.DecodeString(s); len(p) == PublicKeySize {
+		return p
+	}
+	return nil
+}
